92d52401534bd64b0e98ba3b92b0e9079f921376
Casey Verica <cwverica@gmail.com>
2020-07-09 17:23:18 -0700

    Added Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create blog model
+# title
+# publication timestamp
+# body
+# image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    timestamp = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/blog/')
+# add blog app to the settings
+# create a migration
+# migrate
+# add to the admin
--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from .models import Job
 
 # Register your models here.
-from .models import Job
 
 admin.site.register(Job)
